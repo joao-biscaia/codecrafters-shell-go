9abf0943ada64cb35d22ab799c26c909eb08e7c5
joao-biscaia <joaopedro.jpbf@gmail.com>
2025-12-11 11:47:26 -0300

    exit command with switch case

--- app/main.go
+++ app/main.go
@@ -4,10 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
+func exit(splitCommand []string) string {
+	if len(splitCommand) == 2 {
+		exitCode, err := strconv.Atoi(splitCommand[1])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error reading exit code", err)
+			os.Exit(-1)
+		}
+		os.Exit(exitCode)
+	} else if len(splitCommand) == 1 {
+		os.Exit(0)
+	}
+	return "error: too many arguments"
+}
+
+func processInput(inputString string) string {
+	var splitCommand = strings.Split(inputString[:len(inputString)-1], " ")
+	switch splitCommand[0] {
+	case "exit":
+		return exit(splitCommand)
+	default:
+		return splitCommand[0] + ": command not found"
+	}
+
+}
 
 func main() {
 
@@ -18,7 +42,7 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(-1)
 		}
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		fmt.Println(processInput(command))
 	}
 
 }
